a864d33f8e4fc6fe7545a8e39b37ae59d2e9ca69
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2021-06-20 17:20:40 +0530

    phase 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:e_shop/Counters/BookQuantity.dart';
+import 'package:e_shop/Counters/ItemQuantity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'Authentication/authenication.dart';
-import 'Config/config.dart';
+import 'package:e_shop/Config/config.dart';
+import 'Counters/cartitemcounter.dart';
+import 'Counters/changeAddresss.dart';
+import 'Counters/totalMoney.dart';
 import 'Store/storehome.dart';
 
-Future<void>main() async
+Future<void> main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
 
-  EcommerceApp.auth=FirebaseAuth.instance;
+  EcommerceApp.auth = FirebaseAuth.instance;
+  EcommerceApp.sharedPreferences = await SharedPreferences.getInstance();
+  EcommerceApp.firestore = Firestore.instance;
 
   runApp(MyApp());
 }
@@ -21,13 +26,22 @@ Future<void>main() async
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Lucid',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.green,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (c) => CartItemCounter()),
+        ChangeNotifierProvider(create: (c) => ItemQuantity()),
+        ChangeNotifierProvider(create: (c) => AddressChanger()),
+        ChangeNotifierProvider(create: (c) => TotalAmount()),
+      ],
+      child: MaterialApp(
+            title: 'ReBazar',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primaryColor: Colors.green,
+            ),
+            home: SplashScreen()
         ),
-        home: SplashScreen());
+    );
   }
 }
 
@@ -36,53 +50,59 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+
+class _SplashScreenState extends State<SplashScreen>
+{
   @override
   void initState() {
     super.initState();
-
+    
     displaySplash();
   }
 
-  displaySplash() {
-    Timer(Duration(seconds: 4), () async {
-      if (EcommerceApp.auth.currentUser() != null) {
+  displaySplash()
+  {
+    Timer(Duration(seconds: 2), () async {
+      if(await EcommerceApp.auth.currentUser() != null)
+      {
         Route route = MaterialPageRoute(builder: (_) => StoreHome());
         Navigator.pushReplacement(context, route);
-      } else {
+      }
+      else
+      {
         Route route = MaterialPageRoute(builder: (_) => AuthenticScreen());
         Navigator.pushReplacement(context, route);
       }
     });
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Container(
-      decoration: new BoxDecoration(
+      child: Container(
+        decoration: new BoxDecoration(
           gradient: new LinearGradient(
-        colors: [Colors.blue, Colors.deepPurple],
-        begin: const FractionalOffset(0.0, 0.0),
-        end: const FractionalOffset(1.0, 0.0),
-        stops: [0.0, 1.0],
-        tileMode: TileMode.clamp,
-      )),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("images/welcome.png"),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Care  Connect  Share",
-              style: TextStyle(color: Colors.white),
-            )
-          ],
+            colors: [Colors.blue, Colors.deepPurple],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/welcome.png"),
+              SizedBox(height: 20.0,),
+              Text(
+                "SHARE CARE CONNECT",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
